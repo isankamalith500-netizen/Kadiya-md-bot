7a064703db8204097d7737853c1405f71f8c26a3
isankamalith500-netizen <isankamalith500@gmail.com>
2026-07-01 19:44:15 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -1239,20 +1239,20 @@ const downloadQuotedMedia = async (quoted) => {
     // ════════════ PING ════════════
       
     case 'ping': {
-      try { await socket.sendMessage(sender, { react: { text: '🍬', key: msg.key } }); } catch (_) {}     
+      try { await socket.sendMessage(sender, { react: { text: '👥', key: msg.key } }); } catch (_) {}     
       const start = Date.now();
       const ms    = Date.now() - start;
       try { if (pong?.key) await socket.sendMessage(sender, { delete: pong.key }); } catch (_) {}
 
       await socket.sendMessage(sender, {
         image: { url: akira },
-        caption: `*↳ ❝ [🎀 𝗞𝗮𝗱𝗶𝗶𝘆𝗮 𝗠𝗶𝗻𝗶 𝗣𝗶𝗻𝗴 🎀] ¡! ❞*\n\n` +
+        caption: `*↳ ❝ [🎀 𝗞𝗮𝗱𝗶𝘆𝗮 𝗠𝗶𝗻𝗶 𝗣𝗶𝗻𝗴 🎀] ¡! ❞*\n\n` +
              `┏━━━━━°⌜ \`赤い糸\` ⌟°━━━━━┓\n` +
                  `┃₊❏❜ ⋮🏓 𝙿𝙾𝙽𝙶 : _pong!_\n` +
                  `┃₊❏❜ ⋮⚡ 𝚂𝙿𝙴𝙴𝙳 : ${ms}ms\n` +
                  `┃₊❏❜ ⋮⏱️ 𝚄𝙿𝚃𝙸𝙼𝙴 : ${getUptime()}\n` +
              `┗━━━━━°⌜ \`赤い糸 ⌟°━━━━━┛\n\n` +
-                 `> *𝗔esthatic 𝗤ueen 𝗕y 𝗜ꜱᴀɴᴋᴀ 𝜗𝜚⋆*`,
+                 `> *𝗞ᴀᴅɪʏᴀ 𝗤ueen 𝗕y 𝗜ꜱᴀɴᴋᴀ 𝜗𝜚⋆*`,
         contextInfo: arabianCtx()
       }, { quoted: msg });
 
