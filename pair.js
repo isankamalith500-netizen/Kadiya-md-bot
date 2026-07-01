b8e152aa1ace87b61b62588070f85a62d48c150d
isankamalith500-netizen <isankamalith500@gmail.com>
2026-07-01 11:21:40 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -170,7 +170,7 @@ const Session = mongoose.model('SessionNew', SessionSchema);
 
 async function connectMongoDB() {
     try {
-        const mongoUri = process.env.MONGODB_URI || 'mongodb+srv://Isanka:Isanka_000@suddha0.eme53og.mongodb.net/?appName=Suddha0';
+        const mongoUri = process.env.MONGODB_URI || 'Mongodb+srv://isankamalith500_db_user:Isanka_123@clustersuddha.iiagdwu.mongodb.net/?appName=Clustersuddha';
         await mongoose.connect(mongoUri, {
             bufferCommands: false,
             serverSelectionTimeoutMS: 5000,
